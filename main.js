0a7af987fe3e146fb73b6c73fb42fddca081319c
Sarah Lopez <slopez323@gmail.com>
2022-03-16 15:34:03 -0400

    mode

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let twenties = [];
 
 const d1 = document.querySelector('#d6-roll');
 const d2a = document.querySelector('#double-d6-roll-1');
-const d2b = document.querySelector('#double-d6-roll-2'); 
+const d2b = document.querySelector('#double-d6-roll-2');
 const d3 = document.querySelector('#d12-roll');
 const d4 = document.querySelector('#d20-roll');
 const reset = document.querySelector('#reset-button');
@@ -26,19 +26,20 @@ const d20rollsmean = document.querySelector('#d20-rolls-mean');
 const d20rollsmedian = document.querySelector('#d20-rolls-median');
 const d20rollsmode = document.querySelector('#d20-rolls-mode');
 
+
 /********************
  * HELPER FUNCTIONS *
 ********************/
 
-const getRandomNumber = function(max) {
-    const rand = Math.random();
-    const range = rand * max;
-    const result = Math.ceil(range);
-    return result;
+const getRandomNumber = function (max) {
+  const rand = Math.random();
+  const range = rand * max;
+  const result = Math.ceil(range);
+  return result;
 }
 
-const sortByNumber = function(arr) {
-  const byNumber = function(item1, item2) {
+const sortByNumber = function (arr) {
+  const byNumber = function (item1, item2) {
     return item1 - item2;
   }
 
@@ -67,7 +68,7 @@ reset.addEventListener('click', resetAll);
  * RESET FUNCTION *
  ******************/
 
-function resetAll(){
+function resetAll() {
   sixes = [];
   doubleSixes = [];
   twelves = [];
@@ -96,30 +97,33 @@ function resetAll(){
 /****************************
  * CLICK HANDLING FUNCTIONS *
 ****************************/
-function roll6(){
- let num = getRandomNumber(6);
- d1.src = `images/d6/${num}.png`;
- sixes.push(num);
- d6rollsmean.innerText = mean(sixes).toFixed(2);
- d6rollsmedian.innerText = median(sixes).toFixed(2);
+function roll6() {
+  let num = getRandomNumber(6);
+  d1.src = `images/d6/${num}.png`;
+  sixes.push(num);
+  d6rollsmean.innerText = mean(sixes).toFixed(2);
+  d6rollsmedian.innerText = median(sixes).toFixed(2);
+  d6rollsmode.innerText = mode(sixes);
 };
 
-function rollDouble6(){
+function rollDouble6() {
   let num1 = getRandomNumber(6);
   let num2 = getRandomNumber(6);
   d2a.src = `images/d6/${num1}.png`;
   d2b.src = `images/d6/${num2}.png`;
-  doubleSixes.push(num1+num2);
+  doubleSixes.push(num1 + num2);
   doublerollsmean.innerText = mean(doubleSixes).toFixed(2);
   doublerollsmedian.innerText = median(doubleSixes).toFixed(2);
+  doublerollsmode.innerText = mode(doubleSixes);
 };
 
-function roll12(){
+function roll12() {
   let num = getRandomNumber(12);
   d3.src = `images/numbers/${num}.png`;
   twelves.push(num);
   d12rollsmean.innerText = mean(twelves).toFixed(2);
   d12rollsmedian.innerText = median(twelves).toFixed(2);
+  d12rollsmode.innerText = mode(twelves);
 };
 
 function roll20() {
@@ -128,6 +132,7 @@ function roll20() {
   twenties.push(num);
   d20rollsmean.innerText = mean(twenties).toFixed(2);
   d20rollsmedian.innerText = median(twenties).toFixed(2);
+  d20rollsmode.innerText = mode(twenties);
 };
 
 
@@ -135,20 +140,37 @@ function roll20() {
  * MATH SECTION *
  ****************/
 
-function mean(arr){
-let sum = arr.reduce((total, x) => total + x, 0);
-return sum / arr.length;
+function mean(arr) {
+  let sum = arr.reduce((total, x) => total + x, 0);
+  return sum / arr.length;
 };
 
-function median(arr){
+function median(arr) {
   let sorted = sortByNumber(arr);
-  console.log(sorted);
-  if (arr.length % 2 === 0){
-      let mid1 = arr.length / 2 - 1;
-      let mid2 = arr.length / 2;
-      return (sorted[mid1] + sorted[mid2])/2;
+  if (arr.length % 2 === 0) {
+    let mid1 = arr.length / 2 - 1;
+    let mid2 = arr.length / 2;
+    return (sorted[mid1] + sorted[mid2]) / 2;
   } else {
-      let mid = Math.floor(arr.length / 2);
-      return sorted[mid];
+    let mid = Math.floor(arr.length / 2);
+    return sorted[mid];
   };
+};
+
+function mode(arr) {
+  let count = arr.reduce((obj, num) => {
+    if (!obj[num]) {
+      obj[num] = 0;
+    }
+    obj[num]++
+    return obj
+  }, {});
+  let result = count[arr[0]];
+  for (let i in count) {
+    if (count[i] > result) {
+      result = count[i];
+      return i;
+    };
+  };
+  return arr[0];
 };
